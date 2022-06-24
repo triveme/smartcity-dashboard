d66f722326d5aa2a11830e1fa5288ada2fa9dfbd
Faulstich <lf85517@win.edag.de>
2022-06-24 10:03:07 +0200

    add username & password - db connection
    # Conflicts:
    #	backend/frontendService/server.js

--- backend/qlService/server.js
+++ backend/qlService/server.js
@@ -8,7 +8,7 @@ const port = 8081;
 //connect to MongoDB
 mongoose
   .connect(
-    `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
