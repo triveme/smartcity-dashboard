f33f5cf6556bf6f41f3853831fc597443fb6c682
lf85517 <lea.faulstich@edag.com>
2022-06-28 12:48:35 +0200

    remove unnecessary mongo connection to dashboard-collection

--- backend/qlService/app/models/index.js
+++ backend/qlService/app/models/index.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const mongoDB = {};
 
 mongoDB.mongoose = mongoose;
 
-mongoDB.dashboard = require("./dashboard.model");
 mongoDB.querydata = require("./querydata.model");
 
 module.exports = mongoDB;
