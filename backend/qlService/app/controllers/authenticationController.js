712b0d64936bec2a00eac081ef68321dea81f606
Faulstich <lf85517@win.edag.de>
2022-07-01 10:16:21 +0200

    authentication: fix expiration date

--- backend/qlService/app/controllers/authenticationController.js
+++ backend/qlService/app/controllers/authenticationController.js
@@ -48,7 +48,7 @@ function updateToken() {
         process.env.TOKEN = response.data.access_token;
         process.env.REFRESH_TOKEN = response.data.refresh_token;
         let currentTime = new Date().getTime();
-        expirationTime = currentTime + response.date.expires_in - 1500;
+        expirationTime = currentTime + response.data.expires_in * 1000 - 1500;
         console.log(expirationTime);
       }
       /**
