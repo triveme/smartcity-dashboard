fce59f0bf9da6f7735706465823c5aba77736193
Dominik Mack <dominik.mack@edag.com>
2022-10-28 13:25:24 +0200

    switched to default-src self

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,13 +2,14 @@ const { override } = require("customize-cra");
 const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
-  "default-src": "'none'",
+  "default-src": "'self'",
   "base-uri": "'self'",
   "object-src": "'none'",
   "script-src": ["'self'"],
   "style-src": ["'self'"],
   "frame-ancestors": "'self'",
   "form-action": "'self'",
+  "image-src": ["'self'"],
 };
 
 function addCspHtmlWebpackPlugin(config) {
