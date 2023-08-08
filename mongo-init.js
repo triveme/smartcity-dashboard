d12c952a0e362f7a69507ca989d27c13d9fe3093
MarvinHartmann <marvin.hartmann@edag.com>
2023-08-08 14:02:15 +0200

    Production ready docker-compose implementation, WORKING!

--- mongo-init.js
+++ mongo-init.js
@@ -1,5 +1,11 @@
 db = db.getSiblingDB("smartcity-dashboard");
 
+db.createUser({
+  user: "smartcity",
+  pwd: "xbox-anger-amiable2",
+  roles: [{ role: "readWrite", db: "smartcity-dashboard" }],
+});
+
 db.createCollection("dashboards");
 db.createCollection("querydatas");
 db.createCollection("roles");
