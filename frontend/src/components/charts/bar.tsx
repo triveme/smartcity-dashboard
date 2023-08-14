e3c562990697af896c3506190262dbc92fb7e53e
Michael Küchenmeister <122786398+michael-kuechenmeister@users.noreply.github.com>
2023-08-14 09:54:46 +0200

    fix: css-design improvements (#22)
    * fix: css-design improvements
    
    * Fixing issue with MUI Menu complaining about fragments
    
    * Fixing width issue for charts
    
    * Reverting width changes for drawer
    
    ---------
    
    Co-authored-by: MarvinHartmann <marvin.hartmann@edag.com>
    Co-authored-by: MarvinHartmann <114234769+MarvinHartmann@users.noreply.github.com>

--- frontend/src/components/charts/bar.tsx
+++ frontend/src/components/charts/bar.tsx
@@ -153,7 +153,7 @@ export function BarChart(props: BarChartProps) {
   }
 
   return (
-    <Box height='100%' display='flex' alignItems='center' justifyContent='center'>
+    <Box height='100%' width='100%' display='flex' alignItems='center' justifyContent='center'>
       <Box key={'box-' + (tab._id !== '' ? tab._id : tab.uid)} style={{ padding: 5, marginBottom: -15, width: '100%' }}>
         <ApexChart
           key={'apexchart-' + (tab._id !== '' ? tab._id : tab.uid) + tickAmountKey + trigger}
