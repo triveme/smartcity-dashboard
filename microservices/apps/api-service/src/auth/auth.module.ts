d36e3b940d000e4bfd71983e2e4200069f95b3e0
dp85615 <domenik.popfinger@feynsinn.de>
2026-03-23 11:31:01 +0100

    pushed removed files

--- microservices/apps/api-service/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { HttpModule } from '@nestjs/axios';
-
-@Module({
-  imports: [HttpModule],
-  providers: [AuthService],
-})
-export class AuthModule {}
